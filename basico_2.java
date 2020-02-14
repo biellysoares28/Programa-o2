52b1323d0c68408b1b234ddc7da63a2e22b76ddc
gabrielesoares28 <56683895+gabrielesoares28@users.noreply.github.com>
2020-02-14 08:15:33 -0300

    Create basico_2.java

---
+++ basico_2.java
@@ -0,0 +1,10 @@
+package prog2;
+	public class Inicio {
+		public static void main(String[] args) {
+			int x=10; int y=5;
+			int soma= x+y;
+			System.out.printf("A soma de %d + %d é igual a %d",x,y,soma);
+
+	}
+
+}
